4a045cd631aa7c45dc1673617bb17d8f281c7331
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-03-01 16:51:22 +0530

    Create AppTest.java

---
+++ src /test /java /com /example /AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testGetGreeting() {
+        App app = new App();
+        assertEquals("Hello, World!", app.getGreeting());
+    }
+}
